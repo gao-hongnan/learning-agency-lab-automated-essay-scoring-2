a2dc33061c4d29213a421334f0a33c1210345aaf
chuhac <junda_zhu@outlook.com>
2024-06-24 15:37:14 +0800

    feat: add 'compute_metrics_for_reg_cls'

--- lal/src/metrics.py
+++ lal/src/metrics.py
@@ -2,7 +2,7 @@ from typing import Any, Dict
 
 from sklearn.metrics import cohen_kappa_score
 from transformers import EvalPrediction
-
+from scipy.special import softmax 
 
 def compute_metrics_for_classification(eval_pred: EvalPrediction) -> Dict[str, Any]:
     predictions, labels = eval_pred.predictions, eval_pred.label_ids
@@ -14,5 +14,17 @@ def compute_metrics_for_classification(eval_pred: EvalPrediction) -> Dict[str, A
 def compute_metrics_for_regression(eval_pred: EvalPrediction) -> Dict[str, Any]:
     predictions, labels = eval_pred.predictions, eval_pred.label_ids
     eval_qwk = cohen_kappa_score(labels, predictions.clip(0, 5).round(0), weights="quadratic")
+    results = {"eval_qwk": eval_qwk}
+    return results
+
+def compute_metrics_for_reg_cls(eval_pred: EvalPrediction) -> Dict[str, Any]:
+    predictions, labels = eval_pred.predictions, eval_pred.label_ids
+    bs = labels.shape[0]
+    
+    probs = softmax(predictions, axis=-1)
+    indices = np.stack([np.arange(0, 6) for _ in range(bs)])
+    scores = (probs * indices).sum(axis=-1)
+    
+    eval_qwk = cohen_kappa_score(labels, scores.clip(0, 5).round(0), weights="quadratic")
     results = {"eval_qwk": eval_qwk}
     return results
